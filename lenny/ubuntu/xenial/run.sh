cc956f66f87c5c2ed83ba78d713e9546235882b8
fsuarez8 <francisco.suarez@nutonomy.com>
2018-09-18 21:05:41 +0800

    Add lenny docker

---
+++ lenny/ubuntu/xenial/run.sh
@@ -0,0 +1,17 @@
+# Add access control to the X server
+# xhost +local:root
+# Run the docker
+nvidia-docker run -it \
+    --user=$USER \
+    --env="DISPLAY" \
+    --workdir="/home/$USER" \
+    --volume="/home/$USER:/home/$USER" \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    lenny:latest \
+    /bin/bash
+# Remove access control to the X server
+# xhost -local:root
