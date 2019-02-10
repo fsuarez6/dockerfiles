bcf173fcb72d90af9687a32cf0d2080a40c4c330
fsuarez6 <fsuarez6@gmail.com>
2019-02-10 13:19:49 +0800

    Update ros-openrave dockerfile

--- ros-openrave/ubuntu/xenial/build.sh
+++ ros-openrave/ubuntu/xenial/build.sh
@@ -1,1 +1,11 @@
-docker build . -t ros-openrave
+#!/bin/bash
+set -e
+VER=0.1
+TAG=ros-openrave
+
+set -eu
+# Build
+docker build -t ${TAG} .
+# Tag
+docker tag ${TAG} ${TAG}:${VER}
+docker tag ${TAG} ${TAG}:latest
--- ros-openrave/ubuntu/xenial/run.sh
+++ ros-openrave/ubuntu/xenial/run.sh
@@ -1,17 +1,5 @@
-# Add access control to the X server
-# xhost +local:root
-# Run the docker
-nvidia-docker run -it \
-    --user=$USER \
-    --env="DISPLAY" \
-    --workdir="/home/$USER" \
-    --volume="/home/$USER:/home/$USER" \
-    --volume="/etc/group:/etc/group:ro" \
-    --volume="/etc/passwd:/etc/passwd:ro" \
-    --volume="/etc/shadow:/etc/shadow:ro" \
-    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+#!/bin/bash
+docker run -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -it --rm \
+    --privileged --device /dev/dri/ --ipc host --net host  \
     ros-openrave:latest \
     /bin/bash
-# Remove access control to the X server
-# xhost -local:root
