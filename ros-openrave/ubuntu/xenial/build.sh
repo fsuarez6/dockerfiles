53511563285349afdca42c29d8b9160d2b70ce54
fsuarez6 <fsuarez6@gmail.com>
2019-02-10 16:53:21 +0800

    Select new commits for OSG and OpenRAVE

--- ros-openrave/ubuntu/xenial/build.sh
+++ ros-openrave/ubuntu/xenial/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
-VER=0.1
+VER=0.9.0
 TAG=ros-openrave
+REGISTRY=fsuarez6
 
 set -eu
 # Build
 docker build -t ${TAG} .
 # Tag
-docker tag ${TAG} ${TAG}:${VER}
-docker tag ${TAG} ${TAG}:latest
+docker tag ${TAG} ${REGISTRY}/${TAG}:${VER}
+docker tag ${TAG} ${REGISTRY}/${TAG}:latest
